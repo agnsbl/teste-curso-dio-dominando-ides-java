95caeb5fa1963a9755ad58cf23c3e85d59bd799d
agnsbl <mariana.myazbek@gmail.com>
2022-07-11 19:02:51 -0300

    first commit

---
+++ src/com/dio/primeiro/primeiroprograma.java
@@ -0,0 +1,16 @@
+package com.dio.primeiro;
+
+import model.gato;
+
+public class primeiroprograma {
+    public static void main(String[] args) {
+        gato gato = new gato();
+
+        System.out.println(gato);
+
+        /*int a = 5;
+        int b = 3;
+        System.out.println("Hello World!" + (a + b));*/
+    }
+
+}

755c777abde11329d7fa2aa18614ada4c99c949b
agnsbl <mariana.myazbek@gmail.com>
2022-07-12 12:32:49 -0300

    calculadora referente ao exercício do curso "Métodos Java"

---
+++ src/entendendo_metodos_java/Main.java
@@ -0,0 +1,14 @@
+package entendendo_metodos_java;
+
+public class Main {
+
+    public static void main(String[] args) {
+        //Calculadora
+        System.out.println("Exercício calculadora");
+        calculadoradois.soma(2, 3);
+        calculadoradois.subtrai(4, 5);
+        calculadoradois.divide(9, 2);
+        calculadoradois.multiplica(8, 4);
+    }
+
+}
---
+++ src/entendendo_metodos_java/calculadoradois.java
@@ -0,0 +1,33 @@
+package entendendo_metodos_java;
+
+public class calculadoradois {
+
+    public static void soma(double a, double b) {
+        double resultado = a + b;
+
+        System.out.println("A soma de de " + a + " mais " + b + " é igual a " + resultado);
+
+    }
+    public static void subtrai(double a, double b) {
+        double resultado = a - b;
+
+        System.out.println("A subtração de " + a + " menos " + b + " é igual a " + resultado);
+
+    }
+    public static void divide(double a, double b) {
+        double resultado = a / b;
+
+        System.out.println("A divisão de " + a + " por " + b + " é igual a " + resultado);
+
+    }
+    public static void multiplica(double a, double b) {
+        double resultado = a * b;
+
+        System.out.println("A multiplicação de " + a + " com " + b + " é igual a " + resultado);
+
+    }
+
+}
+
+
+
